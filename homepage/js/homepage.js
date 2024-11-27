ee578893bf56f84ca66bd9f90e4c6feb9e6fba95
Stryx-lo <dragonendercat@gmail.com>
2024-11-26 16:38:37 -0800

    toggle stories
    added divs that toggle visibility when each play button is clicked.

--- homepage/js/homepage.js
+++ homepage/js/homepage.js
@@ -12,3 +12,74 @@ $(document).ready(function() {
         });
     });
 });
+
+// Function to show story one when button is clicked
+$(document).ready(function () { 
+    // Click listener
+    $("#storyOneBtn").click(function () { 
+        // shows storyOne div
+        $("#storyOne").toggle("swing"); 
+    }); 
+}); 
+// Function to hide story one when end button is clicked
+$(document).ready(function () { 
+    // Click listener
+    $("#storyOneEnd").click(function () { 
+        // hides storyOne div
+        $("#storyOne").hide("swing"); 
+    }); 
+}); 
+
+
+// Function to show story two when button is clicked
+$(document).ready(function () { 
+    // Click listener
+    $("#storyTwoBtn").click(function () { 
+        // shows storyTwo div
+        $("#storyTwo").toggle("swing"); 
+    }); 
+}); 
+// Function to hide story two when end button is clicked
+$(document).ready(function () { 
+    // Click listener
+    $("#storyTwoEnd").click(function () { 
+        // hides storyTwo div
+        $("#storyTwo").hide("swing"); 
+    }); 
+}); 
+
+
+// Function to show story three when button is clicked
+$(document).ready(function () { 
+    // Click listener
+    $("#storyThreeBtn").click(function () { 
+        // shows storyThree div
+        $("#storyThree").toggle("swing"); 
+    }); 
+}); 
+// Function to hide story three when end button is clicked
+$(document).ready(function () { 
+    // Click listener
+    $("#storyThreeEnd").click(function () { 
+        // hides storyThree div
+        $("#storyThree").hide("swing"); 
+    }); 
+}); 
+
+
+// Function to show story four when button is clicked
+$(document).ready(function () { 
+    // Click listener
+    $("#storyFourBtn").click(function () { 
+        // shows storyFour div
+        $("#storyFour").toggle("swing"); 
+    }); 
+}); 
+// Function to hide story four when end button is clicked
+$(document).ready(function () { 
+    // Click listener
+    $("#storyFourEnd").click(function () { 
+        // hides storyFour div
+        $("#storyFour").hide("swing"); 
+    }); 
+}); 
