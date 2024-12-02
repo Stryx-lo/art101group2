5c4ef5e2279ef8b6810417990b64e80d63104922
Stryx-lo <dragonendercat@gmail.com>
2024-12-01 23:13:33 -0800

    made animation code in js

--- story1/js/story1.js
+++ story1/js/story1.js
@@ -5,8 +5,16 @@
 $("#dogplaceholderone").click(function() {
     // click listener test: works!
     console.log("Image clicked");
-    // Add later: code to make css animate image
+    // Code to make css animate image:
+    // changes to frame 2
+    document.getElementById("dogplaceholderone").src="../story1/img/dogplaceholdertwo.jpg";
+    // waits 0.1 second, then changes to next frame
+    setTimeout(function(){
+        document.getElementById("dogplaceholderone").src="../story1/img/dogplaceholder1.jpg";
+    }, 100);
+    
 })
+
 // listener for image two:
 $("#dogplaceholdertwo").click(function() {
     // click listener test: works!
